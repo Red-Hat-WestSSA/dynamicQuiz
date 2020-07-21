9ecceac7b84a49ac7dd029a8af6f1fbcf1b0d2d8
Bill Novak <bnovak@redhat.com>
2020-07-20 19:09:17 -0700

    Update server.js

--- server.js
+++ server.js
@@ -145,7 +145,7 @@ app.get('/titlesandids', function (req, res) {
 });
 
 
-var server = app.listen(process.env.PORT || 4000, function() {
+var server = app.listen(process.env.PORT || 8080, function() {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
